f4a09f94ffd9f2ed1fb4d5f1a6d2ac501a9c2003
wilber <3605181090101@ingenieria.usac.edu.gt>
2021-08-19 17:36:55 -0600

    Listo para la entrega

--- main.py
+++ main.py
@@ -481,7 +481,7 @@ def generarRepoHTML():
          """
     file.write(end1)
     for para in parameters:
-        if para == "ASC" or " ASC" or "ASC " or " ASC ":
+        if para == "ASC":
             para1 = f"""
             <h3>Listado de forma ASCENDENTE.</h3>
                 <table class="container">
@@ -511,7 +511,97 @@ def generarRepoHTML():
              </table>
              """
             file.write(end2)
-        if para == "DESC" or " DESC" or "DESC " or " DESC ":
+        if para == " ASC":
+            para1 = f"""
+            <h3>Listado de forma ASCENDENTE.</h3>
+                <table class="container">
+                 <thead>
+                 <tr>
+                 <th>
+                 <h1>Nombe del Estudiante</h1>
+                 </th>
+                 <th>
+                 <h1>Nota Obtenida</h1>
+                 </th>
+                 </tr>
+                 </thead>
+                 <tbody> 
+            """
+            file.write(para1)
+            for for2 in ascendente:
+                f2 = f"""
+                <tr>
+                <td>{for2.nombre}</td>
+                <td>{for2.calificacion}</td>
+                </tr>
+                """
+                file.write(f2)
+            end2 = f"""
+             </tbody>
+             </table>
+             """
+            file.write(end2)
+        if para == "ASC ":
+            para1 = f"""
+            <h3>Listado de forma ASCENDENTE.</h3>
+                <table class="container">
+                 <thead>
+                 <tr>
+                 <th>
+                 <h1>Nombe del Estudiante</h1>
+                 </th>
+                 <th>
+                 <h1>Nota Obtenida</h1>
+                 </th>
+                 </tr>
+                 </thead>
+                 <tbody> 
+            """
+            file.write(para1)
+            for for2 in ascendente:
+                f2 = f"""
+                <tr>
+                <td>{for2.nombre}</td>
+                <td>{for2.calificacion}</td>
+                </tr>
+                """
+                file.write(f2)
+            end2 = f"""
+             </tbody>
+             </table>
+             """
+            file.write(end2)
+        if para ==" ASC ":
+            para1 = f"""
+            <h3>Listado de forma ASCENDENTE.</h3>
+                <table class="container">
+                 <thead>
+                 <tr>
+                 <th>
+                 <h1>Nombe del Estudiante</h1>
+                 </th>
+                 <th>
+                 <h1>Nota Obtenida</h1>
+                 </th>
+                 </tr>
+                 </thead>
+                 <tbody> 
+            """
+            file.write(para1)
+            for for2 in ascendente:
+                f2 = f"""
+                <tr>
+                <td>{for2.nombre}</td>
+                <td>{for2.calificacion}</td>
+                </tr>
+                """
+                file.write(f2)
+            end2 = f"""
+             </tbody>
+             </table>
+             """
+            file.write(end2)
+        if para == "DESC":
             para2 = f"""
             <h3>Listado de forma DESCENDENTE.</h3>
                 <table class="container">
@@ -541,7 +631,97 @@ def generarRepoHTML():
              </table>
              """
             file.write(end3)
-        if para == "AVG" or " AVG" or "AVG " or " AVG ":
+        if para ==" DESC":
+            para2 = f"""
+            <h3>Listado de forma DESCENDENTE.</h3>
+                <table class="container">
+                 <thead>
+                 <tr>
+                 <th>
+                 <h1>Nombe del Estudiante</h1>
+                 </th>
+                 <th>
+                 <h1>Nota Obtenida</h1>
+                 </th>
+                 </tr>
+                 </thead>
+                 <tbody> 
+            """
+            file.write(para2)
+            for for3 in descendente:
+                f3 = f"""
+                <tr>
+                <td>{for3.nombre}</td>
+                <td>{for3.calificacion}</td>
+                </tr>
+                """
+                file.write(f3)
+            end3 = f"""
+             </tbody>
+             </table>
+             """
+            file.write(end3)
+        if para ==  "DESC ":
+            para2 = f"""
+            <h3>Listado de forma DESCENDENTE.</h3>
+                <table class="container">
+                 <thead>
+                 <tr>
+                 <th>
+                 <h1>Nombe del Estudiante</h1>
+                 </th>
+                 <th>
+                 <h1>Nota Obtenida</h1>
+                 </th>
+                 </tr>
+                 </thead>
+                 <tbody> 
+            """
+            file.write(para2)
+            for for3 in descendente:
+                f3 = f"""
+                <tr>
+                <td>{for3.nombre}</td>
+                <td>{for3.calificacion}</td>
+                </tr>
+                """
+                file.write(f3)
+            end3 = f"""
+             </tbody>
+             </table>
+             """
+            file.write(end3)
+        if para == " DESC ":
+            para2 = f"""
+            <h3>Listado de forma DESCENDENTE.</h3>
+                <table class="container">
+                 <thead>
+                 <tr>
+                 <th>
+                 <h1>Nombe del Estudiante</h1>
+                 </th>
+                 <th>
+                 <h1>Nota Obtenida</h1>
+                 </th>
+                 </tr>
+                 </thead>
+                 <tbody> 
+            """
+            file.write(para2)
+            for for3 in descendente:
+                f3 = f"""
+                <tr>
+                <td>{for3.nombre}</td>
+                <td>{for3.calificacion}</td>
+                </tr>
+                """
+                file.write(f3)
+            end3 = f"""
+             </tbody>
+             </table>
+             """
+            file.write(end3)
+        if para == "AVG":
             para3 = f"""
             <h3>El PROMEDIO de notas del curso: .</h3>
                 <table class="container">
@@ -554,7 +734,46 @@ def generarRepoHTML():
                  </table>
             """
             file.write(para3)
-        if para == "MIN" or " MIN" or "MIN " or " MIN ":
+        if para ==" AVG":
+            para3 = f"""
+            <h3>El PROMEDIO de notas del curso: .</h3>
+                <table class="container">
+                 <tbody>
+                 <tr>
+                 <td>Promedio</td>
+                 <td>{promedio}</td>
+                 </tr>
+                 </tbody>
+                 </table>
+            """
+            file.write(para3)
+        if para =="AVG ":
+            para3 = f"""
+            <h3>El PROMEDIO de notas del curso: .</h3>
+                <table class="container">
+                 <tbody>
+                 <tr>
+                 <td>Promedio</td>
+                 <td>{promedio}</td>
+                 </tr>
+                 </tbody>
+                 </table>
+            """
+            file.write(para3)
+        if para ==" AVG ":
+            para3 = f"""
+            <h3>El PROMEDIO de notas del curso: .</h3>
+                <table class="container">
+                 <tbody>
+                 <tr>
+                 <td>Promedio</td>
+                 <td>{promedio}</td>
+                 </tr>
+                 </tbody>
+                 </table>
+            """
+            file.write(para3)
+        if para == "MIN":
             para4 = f"""
             <h3>El estudiante con la nota mas BAJA del curso fue: .</h3>
                 <table class="container">
@@ -567,7 +786,46 @@ def generarRepoHTML():
                  </table>
             """
             file.write(para4)
-        if para == "MAX" or " MAX" or "MAX " or " MAX ":
+        if para ==" MIN":
+            para4 = f"""
+            <h3>El estudiante con la nota mas BAJA del curso fue: .</h3>
+                <table class="container">
+                 <tbody>
+                 <tr>
+                 <td>Peor estudiante</td>
+                 <td>{notaMenor}</td>
+                 </tr>
+                 </tbody>
+                 </table>
+            """
+            file.write(para4)
+        if para =="MIN ":
+            para4 = f"""
+            <h3>El estudiante con la nota mas BAJA del curso fue: .</h3>
+                <table class="container">
+                 <tbody>
+                 <tr>
+                 <td>Peor estudiante</td>
+                 <td>{notaMenor}</td>
+                 </tr>
+                 </tbody>
+                 </table>
+            """
+            file.write(para4)
+        if para ==" MIN ":
+            para4 = f"""
+            <h3>El estudiante con la nota mas BAJA del curso fue: .</h3>
+                <table class="container">
+                 <tbody>
+                 <tr>
+                 <td>Peor estudiante</td>
+                 <td>{notaMenor}</td>
+                 </tr>
+                 </tbody>
+                 </table>
+            """
+            file.write(para4)
+        if para == "MAX":
             para5 = f"""
             <h3>El estudiante con la nota mas ALTA del curso fue: .</h3>
                 <table class="container">
@@ -580,7 +838,46 @@ def generarRepoHTML():
                  </table>
             """
             file.write(para5)
-        if para == "APR" or " APR" or "APR " or " APR ":
+        if para == " MAX":
+            para5 = f"""
+            <h3>El estudiante con la nota mas ALTA del curso fue: .</h3>
+                <table class="container">
+                 <tbody>
+                 <tr>
+                 <td>Mejor estudiante</td>
+                 <td>{notaMayor}</td>
+                 </tr>
+                 </tbody>
+                 </table>
+            """
+            file.write(para5)
+        if para =="MAX ":
+            para5 = f"""
+            <h3>El estudiante con la nota mas ALTA del curso fue: .</h3>
+                <table class="container">
+                 <tbody>
+                 <tr>
+                 <td>Mejor estudiante</td>
+                 <td>{notaMayor}</td>
+                 </tr>
+                 </tbody>
+                 </table>
+            """
+            file.write(para5)
+        if para ==" MAX ":
+            para5 = f"""
+            <h3>El estudiante con la nota mas ALTA del curso fue: .</h3>
+                <table class="container">
+                 <tbody>
+                 <tr>
+                 <td>Mejor estudiante</td>
+                 <td>{notaMayor}</td>
+                 </tr>
+                 </tbody>
+                 </table>
+            """
+            file.write(para5)
+        if para == "APR":
             para6 = f"""
             <h3>El numero de estudiantes aprobados fue: {ganadores}.</h3>
             <h3>Listado de estudiantes aprobados: .</h3>
@@ -611,7 +908,193 @@ def generarRepoHTML():
              </table>
              """
             file.write(end3)
-        if para == "REP" or " REP" or "REP " or " REP ":
+        if para ==" APR":
+            para6 = f"""
+            <h3>El numero de estudiantes aprobados fue: {ganadores}.</h3>
+            <h3>Listado de estudiantes aprobados: .</h3>
+                <table class="container">
+                 <thead>
+                 <tr>
+                 <th>
+                 <h1>Nombe del Estudiante</h1>
+                 </th>
+                 <th>
+                 <h1>Nota Obtenida</h1>
+                 </th>
+                 </tr>
+                 </thead>
+                 <tbody> 
+            """
+            file.write(para6)
+            for for3 in listaGanadores:
+                f3 = f"""
+                <tr>
+                <td>{for3.nombre}</td>
+                <td>{for3.calificacion}</td>
+                </tr>
+                """
+                file.write(f3)
+            end3 = f"""
+             </tbody>
+             </table>
+             """
+            file.write(end3)
+        if para =="APR ":
+            para6 = f"""
+            <h3>El numero de estudiantes aprobados fue: {ganadores}.</h3>
+            <h3>Listado de estudiantes aprobados: .</h3>
+                <table class="container">
+                 <thead>
+                 <tr>
+                 <th>
+                 <h1>Nombe del Estudiante</h1>
+                 </th>
+                 <th>
+                 <h1>Nota Obtenida</h1>
+                 </th>
+                 </tr>
+                 </thead>
+                 <tbody> 
+            """
+            file.write(para6)
+            for for3 in listaGanadores:
+                f3 = f"""
+                <tr>
+                <td>{for3.nombre}</td>
+                <td>{for3.calificacion}</td>
+                </tr>
+                """
+                file.write(f3)
+            end3 = f"""
+             </tbody>
+             </table>
+             """
+            file.write(end3)
+        if para ==" APR ":
+            para6 = f"""
+            <h3>El numero de estudiantes aprobados fue: {ganadores}.</h3>
+            <h3>Listado de estudiantes aprobados: .</h3>
+                <table class="container">
+                 <thead>
+                 <tr>
+                 <th>
+                 <h1>Nombe del Estudiante</h1>
+                 </th>
+                 <th>
+                 <h1>Nota Obtenida</h1>
+                 </th>
+                 </tr>
+                 </thead>
+                 <tbody> 
+            """
+            file.write(para6)
+            for for3 in listaGanadores:
+                f3 = f"""
+                <tr>
+                <td>{for3.nombre}</td>
+                <td>{for3.calificacion}</td>
+                </tr>
+                """
+                file.write(f3)
+            end3 = f"""
+             </tbody>
+             </table>
+             """
+            file.write(end3)
+        if para == "REP":
+            para7 = f"""
+            <h3>El numero de estudiantes reprobados fue: {perdedores}.</h3>
+            <h3>Listado de estudiantes reprobados: .</h3>
+                <table class="container">
+                 <thead>
+                 <tr>
+                 <th>
+                 <h1>Nombe del Estudiante</h1>
+                 </th>
+                 <th>
+                 <h1>Nota Obtenida</h1>
+                 </th>
+                 </tr>
+                 </thead>
+                 <tbody> 
+            """
+            file.write(para7)
+            for for4 in listaPerdedores:
+                f4 = f"""
+                <tr>
+                <td>{for4.nombre}</td>
+                <td>{for4.calificacion}</td>
+                </tr>
+                """
+                file.write(f4)
+            end4 = f"""
+             </tbody>
+             </table>
+             """
+            file.write(end4)
+        if para == " REP":
+            para7 = f"""
+            <h3>El numero de estudiantes reprobados fue: {perdedores}.</h3>
+            <h3>Listado de estudiantes reprobados: .</h3>
+                <table class="container">
+                 <thead>
+                 <tr>
+                 <th>
+                 <h1>Nombe del Estudiante</h1>
+                 </th>
+                 <th>
+                 <h1>Nota Obtenida</h1>
+                 </th>
+                 </tr>
+                 </thead>
+                 <tbody> 
+            """
+            file.write(para7)
+            for for4 in listaPerdedores:
+                f4 = f"""
+                <tr>
+                <td>{for4.nombre}</td>
+                <td>{for4.calificacion}</td>
+                </tr>
+                """
+                file.write(f4)
+            end4 = f"""
+             </tbody>
+             </table>
+             """
+            file.write(end4)
+        if para == "REP ":
+            para7 = f"""
+            <h3>El numero de estudiantes reprobados fue: {perdedores}.</h3>
+            <h3>Listado de estudiantes reprobados: .</h3>
+                <table class="container">
+                 <thead>
+                 <tr>
+                 <th>
+                 <h1>Nombe del Estudiante</h1>
+                 </th>
+                 <th>
+                 <h1>Nota Obtenida</h1>
+                 </th>
+                 </tr>
+                 </thead>
+                 <tbody> 
+            """
+            file.write(para7)
+            for for4 in listaPerdedores:
+                f4 = f"""
+                <tr>
+                <td>{for4.nombre}</td>
+                <td>{for4.calificacion}</td>
+                </tr>
+                """
+                file.write(f4)
+            end4 = f"""
+             </tbody>
+             </table>
+             """
+            file.write(end4)
+        if para ==" REP ":
             para7 = f"""
             <h3>El numero de estudiantes reprobados fue: {perdedores}.</h3>
             <h3>Listado de estudiantes reprobados: .</h3>
